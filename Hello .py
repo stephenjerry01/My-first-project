272868eb7216c3c748c7fe203c38ec7b4fd51df1
stephenjerry01 <78447352+stephenjerry01@users.noreply.github.com>
2025-05-11 19:18:28 +0100

    Create Hello .py

---
+++ Hello .py
@@ -0,0 +1,8 @@
+def roast_me():
+    print("Hey Burnie! Be gentle... it's my first time.")
+
+if 5 > 10:  # obviously wrong, but funny
+    print("This will totally run... right?")
+
+if __name__ == "__main__":
+    roast_me()
